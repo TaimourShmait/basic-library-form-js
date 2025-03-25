7ef2305a97ccca7ea0666ea341a0e1a59b9e749c
TaimourShmait <taimourshmaits@gmail.com>
2025-03-25 19:43:21 +0200

    Refined code and added logic for handling radio button inputs

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
 // Main DOM elements and Event Listeners
 
 const mainButtonsContainer = document.querySelector("#main-buttons-container");
-const addBookButton = document.querySelector("#add-book-button");
-const showBooksButton = document.querySelector("#show-books-button");
 const booksContainer = document.querySelector("#books-container");
-
-const addBookDialog = document.querySelector("#add-book-dialog");
 const addBookForm = document.querySelector("#add-book-form");
 
 const bookNameInput = document.querySelector("#book-name");
 const bookAuthorInput = document.querySelector("#book-author");
 const bookPagesInput = document.querySelector("#book-pages");
+const bookGenreInput = document.querySelectorAll('input[name="book-genre"]');
 
-const closeDialogButton = document.querySelector("#close-dialog-button");
-
-addBookButton.addEventListener("click", () => {
-    addBookDialog.showModal(); // Show the dialog box and prevent background interactions, put all focus on the dialog box
-});
-
-showBooksButton.addEventListener("click", () => {
-    showBooks();
-});
+const booksTable = document.querySelector("#books-table");
 
 addBookForm.addEventListener("submit", function(e) {
     e.preventDefault(); // By default, the form will be sent to a sever that does not exist
 
     let bookName = bookNameInput.value;
     let bookAuthor = bookAuthorInput.value;
-    let bookPages = bookPagesInput.value;
+    let bookPages = bookPagesInput.value;   
+    let bookGenre;
 
-    console.log("Submitted " + bookName + ", " + bookAuthor + ", " + bookPages);
+    bookGenreInput.forEach(radio => {if (radio.checked) {bookGenre = radio.value;}});
+
+    console.log("Submitted " + bookName + ", " + bookAuthor + ", " + bookGenre + ", " + bookPages);
 
     bookNameInput.value = "";
     bookAuthorInput.value = "";
     bookPagesInput.value = "";
+    bookGenreInput.forEach(radio => {radio.checked = false});
 
-    addBookDialog.close();
 
-    addBook(bookName, bookAuthor, bookPages);
+    addBook(bookName, bookAuthor, bookPages, bookGenre);
 
 });
-
-closeDialogButton.addEventListener("click", () => {
-    addBookDialog.close();
-});
-
-// Book Class and related functions
 
 class Book {
-    constructor (bookName = "Book", bookAuthor = "John Doe", bookPages = 100) {
+    constructor (bookName = "Book", bookAuthor = "John Doe", bookPages = 100, bookGenre = "Fiction") {
         this.bookName = bookName;
         this.bookAuthor = bookAuthor;
+        this.bookGenre = bookGenre;
         this.bookPages = bookPages;
         this.readStatus = false;
         this.bookId = crypto.randomUUID();
@@ -65,46 +52,54 @@ Book.prototype.toggleReadStatus = function() {this.readStatus = !this.readStatus
 
 const Library = [];
 
-function addBook(bookName, bookAuthor, bookPages) {
-    let book = new Book(bookName, bookAuthor, bookPages);
+function addBook(bookName, bookAuthor, bookPages, bookGenre) {
+    console.log("Reached addBook function");
+    let book = new Book(bookName, bookAuthor, bookGenre, bookPages);
     Library.push(book);
 
     console.log(Library);
+
+    showBooks();
 }
 
 function showBooks() {
 
-    booksContainer.innerHTML = "";
-    
-    let newButtons = document.querySelectorAll(".new-buttons");
-    newButtons.forEach(button => button.remove());
+    booksContainer.style.display = "flex";
+
+    while (booksTable.rows.length > 1) {
+        booksTable.deleteRow(1);
+    }
 
     for (let i = 0; i < Library.length; i++) {
 
-        let bookContainer = document.createElement("div");
-        let bookInformation = document.createElement("p");
+        console.log(Library[i].bookPages + " in the showBooks function!");
 
-        let readStatusText = document.createElement("label");
-        readStatusText.textContent = "Read";
+        let bookContainer = document.createElement("tr");
+        bookContainer.id = "book-container";
+
+        let name = document.createElement("th");
+        let author = document.createElement("th");
+        let pageCount = document.createElement("th");
+        let genre = document.createElement("th");
+        let read = document.createElement("th");
+        
         let readStatusCheckBox = document.createElement("input");
         readStatusCheckBox.setAttribute("type", "checkbox");
 
-        let removeBookButton = document.createElement("button");
-        removeBookButton.textContent = "Remove";
+        read.appendChild(readStatusCheckBox);
 
-        bookInformation.textContent = Library[i].printBookInformation();
+        name.textContent = Library[i].bookName;
+        author.textContent = Library[i].bookAuthor;
+        pageCount.textContent = Library[i].bookPages;
+        genre.textContent = Library[i].bookGenre;
         
-        bookContainer.appendChild(bookInformation);
-        bookContainer.appendChild(readStatusText);
-        bookContainer.appendChild(readStatusCheckBox);
-        bookContainer.appendChild(removeBookButton);
+        bookContainer.appendChild(name);
+        bookContainer.appendChild(author);
+        bookContainer.appendChild(pageCount);
+        bookContainer.appendChild(genre);
+        bookContainer.appendChild(read);
 
-        booksContainer.appendChild(bookContainer);
-
-        removeBookButton.addEventListener("click", () => {
-            bookContainer.remove();
-            Library.splice(i, 1); // Removes the book for the specifc index from the Library array
-        });
+        booksTable.appendChild(bookContainer);
 
         readStatusCheckBox.checked = Library[i].readStatus;
 
@@ -114,32 +109,5 @@ function showBooks() {
         }); 
 
     }
-
-    let clearButton = document.createElement("button");
-    clearButton.textContent = "Clear Page";
-
-    let removeAllButton = document.createElement("button");
-    removeAllButton.textContent = "Remove All";
-    
-    if (Library.length != 0) {
-        mainButtonsContainer.appendChild(clearButton);
-        mainButtonsContainer.appendChild(removeAllButton);
-    }
-        
-    clearButton.classList.add("new-buttons");
-    removeAllButton.classList.add("new-buttons");
-
-    clearButton.addEventListener("click", () => {
-        booksContainer.innerHTML = "";
-        newButtons = document.querySelectorAll(".new-buttons");
-        newButtons.forEach(button => button.remove());
-    });
-
-    removeAllButton.addEventListener("click", () => {
-        booksContainer.innerHTML = "";
-        Library.length = 0;
-        newButtons = document.querySelectorAll(".new-buttons");
-        newButtons.forEach(button => button.remove());
-    });
 
 }
